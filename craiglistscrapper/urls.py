920b142d66a1c585e1f64638d4026eea317b00f7
divincode <devadiga.1@iitj.ac.in>
2021-05-22 13:13:51 +0530

    Set up the views urls app logic and templates

--- craiglistscrapper/urls.py
+++ craiglistscrapper/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from scrapper import views
 
 urlpatterns = [
+    path('', include('scrapper.urls')),
     path('admin/', admin.site.urls),
 ]
